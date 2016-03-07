c9394bdf486f8e588a5d5a284884309f6030ec84
tee <tee_alal@hotmail.com>
2016-03-07 18:12:17 +0900

    modified

--- test_vk/vk/vk.cpp
+++ test_vk/vk/vk.cpp
@@ -1,6 +1,6 @@
 #include <fstream>
 #include <memory>
-
+#include <glm/glm.hpp>
 #include "vk.h"
 
 
@@ -34,6 +34,106 @@ namespace vk
 	Shader fragmentShader;
 
 	UniformBuffer view_proj;
+
+	glm::mat4 frustumMat4(const float left, const float right, const float bottom, const float top, const float nearVal, const float farVal)
+	{
+		if ((right - left) == 0.0f || (top - bottom) == 0.0f || (farVal - nearVal) == 0.0f)
+		{
+			return glm::mat4();
+		}
+
+		glm::mat4 result;
+
+		result[0][0] = 2.0f * nearVal / (right - left);
+		result[0][1] = 0.0f;
+		result[0][2] = 0.0f;
+		result[0][3] = 0.0f;
+		result[1][0] = 0.0f;
+		// Window clip origin is upper left.
+		result[1][1] = -2.0f * nearVal / (top - bottom);
+		result[1][2] = 0.0f;
+		result[1][3] = 0.0f;
+		result[2][0] = (right + left) / (right - left);
+		result[2][1] = (top + bottom) / (top - bottom);
+		result[2][2] = -(farVal + nearVal) / (farVal - nearVal);
+		result[2][3] = -1.0f;
+		result[3][0] = 0.0f;
+		result[3][1] = 0.0f;
+		result[3][2] = -(2.0f * farVal * nearVal) / (farVal - nearVal);
+		result[3][3] = 0.0f;
+
+		return result;
+	}
+
+	glm::mat4 perspectiveMat4(const float fovy, const float aspect, const float zNear, const float zFar)
+	{
+		if (fovy <= 0.0f || fovy >= 180.0f)
+		{
+			return glm::mat4();
+		}
+
+		float xmin, xmax, ymin, ymax;
+
+		ymax = zNear * tanf(glm::radians(fovy * 0.5f));
+		ymin = -ymax;
+		xmin = ymin * aspect;
+		xmax = ymax * aspect;
+
+		return frustumMat4(xmin, xmax, ymin, ymax, zNear, zFar);
+	}
+
+	glm::mat4 lookAtMat4(const float eyeX, const float eyeY, const float eyeZ, const float centerX, const float centerY, const float centerZ, const float upX, const float upY, const float upZ)
+	{
+		glm::vec3 forward;
+		glm::vec3 side;
+		glm::vec3 up;
+
+		forward[0] = centerX - eyeX;
+		forward[1] = centerY - eyeY;
+		forward[2] = centerZ - eyeZ;
+
+		forward = glm::normalize(forward);
+
+		up[0] = upX;
+		up[1] = upY;
+		up[2] = upZ;
+
+		side = glm::cross(forward, up);
+		side = glm::normalize(side);
+
+		up = glm::cross(side, forward);
+
+		glm::mat4 result;
+
+		result[0][0] = side[0];
+		result[0][1] = up[0];
+		result[0][2] = -forward[0];
+		result[0][3] = 0.0f;
+		result[1][0] = side[1];
+		result[1][1] = up[1];
+		result[1][2] = -forward[1];
+		result[1][3] = 0.0f;
+		result[2][0] = side[2];
+		result[2][1] = up[2];
+		result[2][2] = -forward[2];
+		result[2][3] = 0.0f;
+		result[3][0] = 0.0f;
+		result[3][1] = 0.0f;
+		result[3][2] = 0.0f;
+		result[3][3] = 1.0f;
+
+		glm::mat4 translate(1.0f);
+		translate[3][0] = -eyeX;
+		translate[3][1] = -eyeY;
+		translate[3][2] = -eyeZ;
+
+		return result * translate;
+	}
+
+	glm::mat4 lookAtMat4(const glm::vec4& eye, const glm::vec4& center, const glm::vec3& up)
+	{
+		return lookAtMat4(eye.x, eye.y, eye.z, center.x, center.y, center.z, up.x, up.y, up.z);
+	}
 
 	char* load_binary(const char* path, size_t* p_size = nullptr)
 	{
@@ -357,6 +457,16 @@ namespace vk
 
 		if (result == VK_SUCCESS || result == VK_SUBOPTIMAL_KHR)
 		{
+			glm::mat4 projectionMatrix(1.0f);
+			glm::mat4 viewMatrix(1.0f);
+
+			projectionMatrix = perspectiveMat4(45.0f, (float)1024 / (float)768, 1.0f, 100.0f);
+			viewMatrix = lookAtMat4(0.0f, 4.0f, 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+
+			view
+
+
 			VkPipelineStageFlags waitDstStageMask = VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT;
 
 			VkSubmitInfo submitInfo = {};
